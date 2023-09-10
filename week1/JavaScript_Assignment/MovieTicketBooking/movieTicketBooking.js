d29cfb4e8e61872da51a8c462096be58e2182eac
spcrs <100281603+spcrs@users.noreply.github.com>
2023-09-10 13:27:24 +0530

    movie ticket booking problem some functionality added

--- week1/JavaScript_Assignment/MovieTicketBooking/movieTicketBooking.js
+++ week1/JavaScript_Assignment/MovieTicketBooking/movieTicketBooking.js
@@ -29,9 +29,37 @@ function createSeats() {
 
 
 //show alert when user confirms
-function showAlert() {
-  var result = confirm("Do you want to proceed?");  
-  return result;
+function showAlert(msg,btn1,btn2,cb1,cb2) {
+  console.log("show alert function called")
+  document.querySelector("#alert").style.display = "flex";
+  console.log(document.querySelector("#alert-msg"));
+  document.querySelector("#alert-msg").innerHTML = msg;
+  document.querySelector("#btn1").innerHTML = btn1;
+  if(btn2 === undefined)
+    document.querySelector("#btn2").style.display = "none";
+  else{
+    document.querySelector("#btn2").style.display = "inline-block";
+    document.querySelector("#btn2").innerHTML = btn2;
+    document.querySelector("#btn2").addEventListener("click", btn2ClickEvent);
+  }
+  function btn1ClickEvent(){
+      document.querySelector("#alert").style.display = "none";    
+      cb1();
+      document.querySelector("#btn1").removeEventListener("click", btn1ClickEvent);
+      document.querySelector("#btn2").removeEventListener("click", btn2ClickEvent);
+
+  }
+
+  function btn2ClickEvent(){
+      document.querySelector("#alert").style.display = "none";
+      cb2();
+      document.querySelector("#btn2").removeEventListener("click", btn2ClickEvent);
+      document.querySelector("#btn1").removeEventListener("click", btn1ClickEvent);
+
+  }
+
+  document.querySelector("#btn1").addEventListener("click", btn1ClickEvent);
+
 }
 
 
@@ -64,11 +92,27 @@ function isValidSeatNumber(seat_number){
   return true;
 }
 
+function clearInputfields(){
+  document.querySelector("#no-of-seats").value = "";
+  document.querySelector("#seats-number").value = "";
+}
+
+function exit(){
+  document.querySelector("#book-your-ticket").style.display = "block";
+  document.querySelector("#book-ticket").style.display = "none";
+}
+
+function continueBooking(){
+  showAlert("Do you want to continue booking?","Yes","No",cb1=checkSeatNumber,cb2=restartOrExit)
+}
+
+function restartOrExit(){
+  showAlert("do you want to restart or exit?","Restart","Exit",cb1=clearInputfields,cb2=exit)
+}
 
 
-//book ticket button click event
-book_ticket_button.addEventListener('click', function(e) {
-  e.preventDefault();
+//check seat numbers are valid or not and number of seats and seats number are equal or not and seat is already booked or not
+function checkSeatNumber(){
   
   const no_of_seats = document.querySelector('#no-of-seats').value;
   const seats_number = document.querySelector('#seats-number').value.trim().split(",");
@@ -82,16 +126,12 @@ book_ticket_button.addEventListener('click', function(e) {
       return;
     }
   }
-  
-
 
   //check number of seats and seats number are equal or not
   if(no_of_seats != seats_number.length){
     alert('number of seats and seats number are not equal');
     return;
   }
-
-
 
   //check seat is already booked or not
   let booked_seats = [];
@@ -108,26 +148,29 @@ book_ticket_button.addEventListener('click', function(e) {
     return;
   }
 
+  //book the seats
+  for(let seat_number of seats_number){
+    seat_number = seat_number.trim();
+    const row = seat_number[0] - 1;
+    const col = seat_number[1] - 1;
+    bookSeat(row,col);
+  }
+
+  //total amount
+  const total_amount = no_of_seats * 120;
+  showAlert(`Amount to be Paid ${total_amount}`,"Pay",undefined,cb1=bookAgain)
+}
 
 
-  //confrim the booking
-  if(showAlert()){
-    for(let seat_number of seats_number){
-      seat_number = seat_number.trim();
-      const row = seat_number[0] - 1;
-      const col = seat_number[1] - 1;
-      if(bookSeat(row,col) === false) {
-        alert('Seat is already booked');
-        return;
-      }
-      bookSeat(row,col);
-    }
-  }
-  else{
-    document.querySelector("#book-your-ticket").style.display = "flex";
-    document.querySelector("#book-ticket").style.display = "none";
-  }
-  
+function bookAgain(){
+  showAlert("Do you want to Book more tickets?","Yes","No",cb1=clearInputfields,cb2=exit)
+}
+
+
+//book ticket button click event
+book_ticket_button.addEventListener('click', function(e) {
+  e.preventDefault();
+  continueBooking();
 });
 
 
